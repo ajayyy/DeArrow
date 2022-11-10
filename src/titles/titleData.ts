0115e30919757129ee29400f3cfa21f83f236952
Ajay <dev@ajay.app>
2022-11-10 12:38:04 -0500

    Add title case when title not found

--- src/titles/titleData.ts
+++ src/titles/titleData.ts
@@ -1,6 +1,6 @@
 import { VideoID } from "../videoBranding/videoBranding";
 
 // eslint-disable-next-line require-await, @typescript-eslint/no-unused-vars
-export async function getTitle(videoID: VideoID): Promise<string> {
-    return "Some title goes here";
+export async function getTitle(videoID: VideoID): Promise<string | null> {
+    return null;
 }
--- src/titles/titleRenderer.ts
+++ src/titles/titleRenderer.ts
@@ -8,10 +8,26 @@ export async function replaceTitle(element: HTMLElement, videoID: VideoID): Prom
     titleElement.style.visibility = "hidden";
 
     const title = await getTitle(videoID);
+    console.log(title, titleElement.textContent, titleElement.innerHTML)
     if (title) {
         titleElement.innerText = title;
+    } else if (titleElement.textContent) {
+        // TODO: Allow customizing this rule
+        // innerText is blank when visibility hidden
+        titleElement.innerText = toTitleCase(titleElement.textContent);
     }
 
     titleElement.style.visibility = "visible";
     return true;
+}
+
+// https://stackoverflow.com/a/196991
+function toTitleCase(str: string): string {
+    // TODO: ignore some acronyms like AI, allow customizing
+    return str.replace(
+        /\w\S*/g,
+        (txt) => {
+            return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+        }
+    );
 }
