be17528d9d5f1d8cb52ab25f0f49916410f23065
Ajay <dev@ajay.app>
2023-05-10 00:00:57 -0400

    Wait for config to be ready in popup
    Fixes popup on firefox

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { PopupComponent } from "./PopupComponent";
+import { waitFor } from "@ajayyy/maze-utils";
+import Config from "../config";
 
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+    await waitFor(() => Config.isReady());
+
     const root = createRoot(document.body);
     root.render(<PopupComponent/>);
 })
