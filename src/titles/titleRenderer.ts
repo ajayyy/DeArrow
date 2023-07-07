ae72bfdabb1c807f8894d2e757e12a6af46a38e6
Ajay <dev@ajay.app>
2023-07-06 22:54:48 -0400

    Fix double show original button sometimes appearing

--- src/titles/titleRenderer.ts
+++ src/titles/titleRenderer.ts
@@ -296,7 +296,7 @@ export async function hideAndUpdateShowOriginalButton(element: HTMLElement, bran
 
 export async function findShowOriginalButton(originalTitleElement: HTMLElement, brandingLocation: BrandingLocation): Promise<HTMLElement> {
     const referenceNode = brandingLocation === BrandingLocation.Watch 
-        ? (await getOrCreateTitleButtonContainer(originalTitleElement.parentElement!)) : originalTitleElement.parentElement;
+        ? (await getOrCreateTitleButtonContainer())?.parentElement : originalTitleElement.parentElement;
     return referenceNode?.querySelector?.(".cbShowOriginal") as HTMLElement;
 }
 
