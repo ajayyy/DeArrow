c7139b8e22d0d0ceccc8689b5a62513b9fd872e5
Ajay <dev@ajay.app>
2023-07-29 01:12:58 -0400

    Don't run page cleanup when not needed

--- src/utils/pageCleaner.ts
+++ src/utils/pageCleaner.ts
@@ -1,6 +1,8 @@
 export function cleanPage() {
     // For live-updates
-    for (const element of document.querySelectorAll(".cbShowOriginal, .cb-css")) {
-        element.remove();
+    if (document.readyState === "complete") {
+        for (const element of document.querySelectorAll(".cbShowOriginal, .cb-css")) {
+            element.remove();
+        }
     }
 }
