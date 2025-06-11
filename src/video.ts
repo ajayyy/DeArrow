7ecb57f192728455bef7e7bba497aa3a02b9649f
Ajay <dev@ajay.app>
2025-06-11 01:15:07 -0400

    Fix related videos becoming blank

--- src/video.ts
+++ src/video.ts
@@ -3,7 +3,7 @@ import { logError } from "./utils/logger";
 import { ChannelIDInfo, checkIfNewVideoID, getVideoID, isOnYTTV, setupVideoModule, VideoID } from "../maze-utils/src/video"
 import Config from "./config/config";
 import { SubmitButton } from "./submission/submitButton";
-import { BrandingLocation, BrandingResult, clearVideoBrandingInstances, replaceCurrentVideoBranding } from "./videoBranding/videoBranding";
+import { BrandingLocation, BrandingResult, clearVideoBrandingInstances, replaceCurrentVideoBranding, updateBrandingForAllVideos } from "./videoBranding/videoBranding";
 import { getVideoBranding } from "./dataFetching";
 import * as documentScript from "../dist/js/document.js";
 import { listenForBadges, listenForMiniPlayerTitleChange, listenForTitleChange } from "./utils/titleBar";
@@ -129,6 +129,13 @@ export function setupCBVideoModule(): void {
             setTimeout(() => {
                 replaceCurrentVideoBranding().catch(logError);
             }, 100);
+        });
+
+        window.addEventListener("resize", () => {
+            // Fix related videos becoming blank
+            setTimeout(() => {
+                updateBrandingForAllVideos();
+            }, 500);
         })
     }
 }
