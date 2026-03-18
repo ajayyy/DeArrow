0eb92c4a297b13afca8661e74748fa4fb535827a
Ajay <dev@ajay.app>
2026-03-18 05:21:50 -0400

    Fix requests not working

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -3,5 +3,5 @@ import Config from "../config/config";
 import * as CompileConfig from "../../config.json";
 
 export function sendRequestToServer(type: string, url: string, data = {}): Promise<FetchResponse> {
-    return sendRequestToCustomServer(type, Config.config?.serverAddress ?? CompileConfig.serverAddress + url, data);
+    return sendRequestToCustomServer(type, (Config.config?.serverAddress ?? CompileConfig.serverAddress) + url, data);
 }
